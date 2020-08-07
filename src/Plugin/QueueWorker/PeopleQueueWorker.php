6acd97cea0eaadf6dc18d4e02fc90db4f6570432
Raranchik <raranchik007@gmail.com>
2020-08-07 17:03:53 +0700

    Changes Queue

--- src/Plugin/QueueWorker/PeopleQueueWorker.php
+++ src/Plugin/QueueWorker/PeopleQueueWorker.php
@@ -6,20 +6,16 @@
   use Drupal\node\Entity\Node;
 
   /**
-   * Processes Node Tasks.
+   * A Node Publisher that publishes nodes on CRON run.
    *
    * @QueueWorker(
    *   id = "people_queue_worker",
-   *   title = @Translation("Task Worker: Node"),
+   *   title = @Translation("Task worker: Node"),
    *   cron = {"time" = 60}
    * )
    */
 
   class PeopleQueueWorker extends QueueWorkerBase {
-    public function existsNode($node) {
-
-    }
-
     public function processItem($Character) {
       $node = Node::create([
         'type' => 'people',
@@ -27,13 +23,7 @@
       ]);
 
       foreach ($Character as $field => $fieldValue) {
-        if (!is_array($fieldValue)) {
-          $node->set(('field_' . $field), $fieldValue);
-        }
-
-        foreach ($fieldValue as $list) {
-          $node->set(('field_' . $field), $list);
-        }
+        $node->set(('field_' . $field), $fieldValue);
       }
 
       $node->setPublished(true);
